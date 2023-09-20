6b1a4063f8bf6f4f06591abd6e1291c65c40610d
vincent-backend <email:yutaka@inc-acv.com>
2023-09-20 20:00:24 +0700

    Update Banner-Statistics

--- layouts/components/banner/BannerStatistics.js
+++ layouts/components/banner/BannerStatistics.js
@@ -1,7 +1,13 @@
 const BannerStatistics = () => {
     return(
         <div className="banner-statistics">
-            <div className="book" />
+            <div className="book">
+                <div className="book-title" />
+                <div className="trangulate" />
+                <div className="dot-green" />
+                <div className="dot-yellow" />
+                <div className="dot-blue" />
+            </div>
             <div className="display">
                 <div className="display-inner" />
             </div>
