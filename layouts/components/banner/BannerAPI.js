5c5e4a0e6c1ed8744d105ecae248435ffd271afe
vincent-backend <email:yutaka@inc-acv.com>
2023-09-21 11:26:12 +0700

    API Banner Static Design

--- layouts/components/banner/BannerAPI.js
+++ layouts/components/banner/BannerAPI.js
@@ -3,12 +3,14 @@ const BannerAPI = () => {
       <div className="banner-api">
         <div className="right-base" />
         <div className="right-pipe">
-            <div className="slide" />
+          <div className="slide" />
         </div>
         <div className="central-bottom" />
         <div className="central-top" />
+        <div className="left_pipe">
+          <div className="slide" />
+        </div>
         <div className="computer">
-            <div className="main-board" />
             <div className="display" />
         </div>
         <div className="belt">
