5a0176cd1759ec353e25344bfc14fd53ee47ad22
vincent-backend <email:yutaka@inc-acv.com>
2023-09-30 16:39:38 +0700

    Fixed Copy to clipboard issue on Firefox

--- hooks/useClipboard.js
+++ hooks/useClipboard.js
@@ -14,16 +14,31 @@ const copyToClipboard = (text) => {
             notify("success", "The url has been copied.");
         }
         })
-        .catch(error => {
-        if(window.ffclipboard) {
-            ffclipboard.setText(text);
-        }
-        else {
-            console.log(error);
-            notify("error", "Clipboard is not avaliable in this web browser.");
-        }
-        
+        .catch (async (error) => {
+            const input = document.createElement("input");
+            input.value = text;
+            input.style.position = "fixed";
+            input.style.top = "-2000px";
+            document.body.appendChild(input);
+            input.select();
+            try {
+                await new Promise((resolve) => {
+                  if (document.execCommand("copy")) {
+                    resolve();
+                  }
+                });
+            
+                document.body.removeChild(input);
+                notify("success", "The url has been copied.");
+              } 
+              catch (err) {
+                console.log(error);
+                notify("error", "Clipboard is not avaliable in this web browser.");
+            }
         });	
+    }
+    else {
+        console.log("Error");
     }
 };
 
