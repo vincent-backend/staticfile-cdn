079bfeb97f28eee213f7e7c1e7b72d4da8f1ba43
vincent-backend <email:yutaka@inc-acv.com>
2023-10-10 08:28:19 +0700

    Home Update... Loading.svg on File list

--- layouts/components/home/LibraryView.js
+++ layouts/components/home/LibraryView.js
@@ -8,9 +8,12 @@ import useTranslation from "@hooks/useTranslation";
 import { notify } from "@hooks/useNotify";
 import CopyToClipboard from "@hooks/useClipboard";
 
+import Image from "next/image";
+
 const LibraryView = ({ libData, section }) => {
   const { locale, setLocale } = useTranslation();
 
+  const [isFilesLoading, setFilesLoading] = useState(false);
   const [isLoaded, setLoaded] = useState(libData.rslt);
 
   const [fileList, setFileList] = useState(libData.files);
@@ -19,11 +22,13 @@ const LibraryView = ({ libData, section }) => {
 
   const handleVersionChange = async (version) => {
     setVersion(version);
+    setFilesLoading(true);
     const { rslt, ...res } = await getFileList(libname, version);
     setLoaded(rslt);
     if (rslt == true) {
       setFileList(res.files);
     }
+    setFilesLoading(false);
   };
 
   const renderListItem = (http, filename, i) => {
@@ -71,22 +76,32 @@ const LibraryView = ({ libData, section }) => {
               </select>
             </div>
           </div>
-          <div>
+          <div className="flex-col">
             <Tabs>
               <Tab name="HTTPS">
                 <ul role="list" className="home-tab-ul">
-                  {fileList &&
+                  {!isFilesLoading && fileList &&
                     fileList.map((file, index) =>
                       renderListItem("https", file, index),
                     )}
+                  {isFilesLoading && 
+                  <div className="flex flex-col items-center justify-center">
+                    <Image src="/images/loading.svg" alt="Loading data" width={200} height={200} />
+                  </div> 
+                  }
                 </ul>
               </Tab>
               <Tab name="HTTP">
                 <ul role="list" className="home-tab-ul">
-                  {fileList &&
+                {!isFilesLoading && fileList &&
                     fileList.map((file, index) =>
                       renderListItem("http", file, index),
                     )}
+                  {isFilesLoading && 
+                  <div className="flex flex-col h-[200px] items-center justify-center">
+                    <Image src="/images/loading.svg" alt="Loading data" width={200} height={200} />
+                  </div> 
+                  }
                 </ul>
               </Tab>
             </Tabs>
@@ -109,7 +124,7 @@ const LibraryView = ({ libData, section }) => {
           </div>
         </div>
         <div className="online-store">
-          <a href={isLoaded && libData.homepage}>{section.homepage}</a>
+          <a href={isLoaded ? libData.homepage : "#"}>{section.homepage}</a>
         </div>
       </div>
     </div>
