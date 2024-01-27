32ad51d8cf787f667443f3a031a535e528f527a7
brian-backend <brian@funnull.com>
2024-01-27 11:21:49 +0700

    env

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    API_HOST: process.env.API_HOST,
+    API_STATISTICS: process.env.API_STATISTICS
+  },
   reactStrictMode: true,
   output: "export"
 };
